1f8590c25c37f126fd3d6e1869164c165c5b2b9c
Yuheng Li <howard@cfins-dreamseat.(none)>
2013-06-26 15:51:55 +0800

    change the line feed to \r\n to fit the format of csv file

--- importgztab.sh
+++ importgztab.sh
@@ -2,4 +2,4 @@
 pipe=`realpath $4`
 db_name=$2
 zcat $1 >$4&
-mysql -uroot --show-warnings --local-infile -e "LOAD DATA LOCAL INFILE '$pipe' INTO TABLE $3 FIELDS TERMINATED BY '\\t' LINES TERMINATED BY '\\n'" $db_name
+mysql -uroot --show-warnings --local-infile -e "LOAD DATA LOCAL INFILE '$pipe' INTO TABLE $3 FIELDS TERMINATED BY '\\t' LINES TERMINATED BY '\\r\\n'" $db_name
