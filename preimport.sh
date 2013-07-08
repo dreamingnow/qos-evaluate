a5d58b34ef0fbdccc9d65a86d18759eff7bb64a2
Yuheng Li <Howard@DreamTablet>
2013-07-08 11:51:50 +0800

    minor fix of preimport batch script

--- preimport.sh
+++ preimport.sh
@@ -3,6 +3,6 @@
 pipe=`realpath $3`
 db_name=$2
 echo Import through named pipe $pipe
-zcat $1 | awk 'BEGIN{FS=OFS="\t"} {if ($12 ~ /Apple/) {if ($12 ~ /iPad/) $12="ios-large"; else $12="ios-small";} else $12="an"; print;}' >$3&
+zcat $1 | awk 'BEGIN{FS=OFS="\t"} {if ($12 ~ /Apple/) {if ($12 ~ /iPad/) $12="ios-large"; else $12="ios-small";} else $12="an"; print;}' >$pipe&
 mysql -uroot --show-warnings --local-infile -e "LOAD DATA LOCAL INFILE '$pipe' INTO TABLE seg_log FIELDS TERMINATED BY '\\t' LINES TERMINATED BY '\\n'" $db_name
 
